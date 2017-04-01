400635cbc2cba24fb50c16ae935d6ff0a9943d7c
Anupam Basak <anupam.basak27@gmail.com>
2017-04-02 01:53:03 +0530

    Major Changes to Internal Structure

--- src/Annotation/index.js
+++ src/Annotation/index.js
@@ -5,7 +5,13 @@ class Annotation {
 
     value() { return this.value; }
 
-    set(name, value) { this[name] = value; }
+    set(name, value) {
+        if (name !== "value" && typeof this[name] !== "function") {
+            throw new Error("Invalid Argument")
+        } else {
+            this[name] = value;
+        }
+    }
 }
 
 module.exports = Annotation;
