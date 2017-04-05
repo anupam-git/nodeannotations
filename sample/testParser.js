35a09e0e9d366e06a3eacea63b5aefe6b18c6cf4
Anupam Basak <anupam.basak27@gmail.com>
2017-04-05 18:56:11 +0530

    Update to support Synchronous Parsing

--- sample/testParser.js
+++ sample/testParser.js
@@ -1,26 +1,23 @@
 const AnnotationParser = require(__dirname+"/../").AnnotationParser;
 const Request = require(__dirname+"/myannotations/Request");
 
-AnnotationParser.parse(
-    __dirname+"/annotatedFile.js",
-    __dirname+"/myannotations/",
-    (err, annotatedElements) => {
-        if (err) {
-            console.log(err);
-        } else {
-            for (let i in annotatedElements) {
-                if (typeof annotatedElements[i] != "function") {        // TODO : Improve this feature
-                    let annotatedElement = annotatedElements[i];
-                    console.log(annotatedElement.getName()+" : "+annotatedElement.getType());
+try {
+    let annotatedElements = AnnotationParser.parse(__dirname+"/annotatedFile.js", __dirname+"/myannotations/");
 
-                    let elementAnnotations = annotatedElement.getAnnotations();
+    for (let i in annotatedElements) {
+        if (typeof annotatedElements[i] != "function") {        // TODO : Improve this feature
+            let annotatedElement = annotatedElements[i];
+            console.log(annotatedElement.getName()+" : "+annotatedElement.getType());
 
-                    for (let i in elementAnnotations) {
-                        console.log("\t"+JSON.stringify(elementAnnotations[i]));
-                    }
+            let elementAnnotations = annotatedElement.getAnnotations();
 
-                    console.log();
-                }
+            for (let i in elementAnnotations) {
+                console.log("\t"+JSON.stringify(elementAnnotations[i]));
             }
+
+            console.log();
         }
-    });
+    }
+} catch (err) {
+    console.log(err);
+}
